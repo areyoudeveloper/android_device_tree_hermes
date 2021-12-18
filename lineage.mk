5a58afcfa04091e107f8e57bde8d3c7b597952bf
Darkness Hidden(TEA) <kmd36rk@gmail.com>
2021-12-18 21:45:10 +0700

    Update lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -31,8 +31,8 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 2
 PRODUCT_MANUFACTURER := Xiaomi
 
-ifeq ($(TARGET_BUILD_VARIANT),user)
+ifeq ($(TARGET_BUILD_VARIANT),userdebug)
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-BUILD_FINGERPRINT := "Xiaomi/hermes/hermes:5.0.2/LRX22G/V9.6.3.0.LHMMIFD:user/release-keys"
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="hermes-user 5.0.2 LRX22G V9.6.3.0.LHMMIFD release-keys"
+BUILD_FINGERPRINT := "Xiaomi/hermes/hermes:5.0.2/LRX22G/V9.6.3.0.LHMMIFD:userdebug/release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="hermes-userdebug 5.0.2 LRX22G V9.6.3.0.LHMMIFD release-keys"
 endif
